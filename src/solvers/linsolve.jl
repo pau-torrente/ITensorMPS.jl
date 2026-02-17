a289874d9b2bfbf6bd5efb6d30b2a04210ce1941
pau-torrente <ptbadia@gmail.com>
2026-02-17 14:57:13 +0100

    formatting

--- src/solvers/linsolve.jl
+++ src/solvers/linsolve.jl
@@ -17,8 +17,8 @@ function krylov_updater(problem::ReducedPrecondLinearProblem, init; internal_kwa
     x, info = linsolve(
         operator(problem.linear_problem),
         constant_term(problem.linear_problem),
-	problem.preconditioner,
         init,
+        problem.preconditioner,
         coefficients[1],
         coefficients[2];
         kwargs...,
